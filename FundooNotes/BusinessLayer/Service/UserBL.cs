2861b88b77019ca170f538090aa935d34359da3d
sagunaukade <sagunaukade96@gmail.com>
2022-03-11 00:23:27 +0530

    [Saguna] Added FundooNotes Project

---
+++ FundooNotes/BusinessLayer/Service/UserBL.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class UserBL : IUserBL
+    {
+        private readonly IUserRL userRL;
+
+        public UserBL(IUserRL userRL)
+        {
+            this.userRL = userRL;
+
+        }
+        public UserEntity Registration(UserRegistration User)
+        {
+            try
+            {
+                return userRL.Resistration(User);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
+      
